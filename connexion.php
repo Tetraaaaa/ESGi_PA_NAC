67a6392da3676cf99e329c7c04ddb41b76282621
Adrien Meslin <adn.meslin@gmail.com>
2024-05-24 12:12:11 +0200

    Modification CSS

--- connexion.php
+++ connexion.php
@@ -3,38 +3,30 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ma page d'accueil</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
+  <title>Connexion</title>
+  <link rel="stylesheet" href="css/connexion.css">
 </head>
-<body class="bg-blue">
+<body>
 
-<?php require_once 'include/connection_db.php'; ?>
-<!-- Git fonctionne--->
-<main>
-    <form method="POST" class="my-login-validation" action="connexion_verif.php">
-        <div class="form-group">
-            <label for="email">Adresse E-Mail</label>
-            <input id="email" type="email" class="form-control" name="email" required>
-        </div>
-
-        <div class="form-group">
-            <label for="password">Mot de passe</label>
-            <input id="password" type="password" class="form-control" name="password" required>
-        </div>
-
-        <div>
-            <input type="checkbox" class="form-check-input" id="stayLoggedIn" name="stayLoggedIn">
-            <label class="form-check-label" for="stayLoggedIn">Rester connecté</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Connexion</button>
-        <div>
-            <button type="button" class="btn btn-secondary" onclick="window.location.href='inscription.php';">Inscription</button>
-        </div>
+<div class="container">
+  <div class="login-box">
+    <div class="avatar">
+      <img src="image/PCS.png" alt="Avatar">
+    </div>
+    <h2>Connexion</h2>
+    <form method="POST" action="connexion_verif.php">
+      <div class="inputBox">
+        <input type="text" id="email" name="email" placeholder="Email" required>
+      </div>
+      <div class="inputBox">
+        <input type="password" id="password" name="password" placeholder="Password" required>
+      </div>
+      <div class="inputBox">
+        <input type="submit" value="Se connecter">
+      </div>
     </form>
-</main>
+  </div>
+</div>
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
